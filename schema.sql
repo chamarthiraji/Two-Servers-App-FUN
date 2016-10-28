df40fc3434fc0aabb580b02b744844b34c56f15a
Rajyalakshmi C <chamarthi226@gmail.com>
2016-10-28 17:28:51 -0500

    schema for products,departments added

--- schema.sql
+++ schema.sql
@@ -11,3 +11,19 @@ CREATE TABLE products(
 	);
 	
 	SELECT * FROM products;
+
+
+USE bamazon;
+CREATE TABLE departments(
+	deptID INTEGER AUTO_INCREMENT NOT NULL,
+	dept VARCHAR(15) NOT NULL,
+	overHeadCosts FLOAT(10,2) NOT NULL,
+	totalSales FLOAT(10,2) ,
+	PRIMARY KEY (deptID)
+	);
+SELECT * FROM departments;
+
+INSERT INTO departments(dept,overHeadCosts)
+VALUES('Toys','500');
+INSERT INTO departments(dept,overHeadCosts)
+VALUES('Electronics','200'),('HomeFurniture',1000),('Clothing',100);
