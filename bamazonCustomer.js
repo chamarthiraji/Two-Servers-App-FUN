00307085814da0dd864c9d1ce20105f69358e414
Rajyalakshmi C <chamarthi226@gmail.com>
2016-10-28 17:27:19 -0500

    bamazon customer file added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var tempres ;
+require('console.table');
+var tempres,tprice ;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,9 +18,10 @@ connection.connect(function(err) {
 
 connection.query('SELECT * FROM products', function(err, res) {
     for (var i = 0; i < res.length; i++) {
-        console.log(res[i].itemID + " | " + res[i].productName + " | " + res[i].price + " | " + res[i].stockQuantity);
+
+        //console.log(res[i].itemID + " | " + res[i].productName + " | " + res[i].price + " | " + res[i].stockQuantity);
     }
-    console.log("-----------------------------------");
+    console.table(res);
 
     inquirer.prompt([
         {
@@ -40,75 +42,37 @@ connection.query('SELECT * FROM products', function(err, res) {
         console.log("answer.quantity ",answer.quantity);
         console.log("res[answer.pId-1].stockQuantity",res[answer.pId-1].stockQuantity);
 
+
         if (answer.quantity > res[answer.pId-1].stockQuantity) {
             console.log("Insufficient Quantity ");
         }
         else{
+          var  remainingStock = res[answer.pId-1 ].stockQuantity - answer.quantity;
+            tprice = answer.quantity * res[answer.pId-1 ].price ;
             console.log("inside else");
             console.log("stockQuantity updated : ",res[answer.pId-1 ].stockQuantity - answer.quantity);
-            connection.query("UPDATE products SET ? WHERE ?", [{
-                    stockQuantity: res[answer.pId-1].stockQuantity - answer.quantity
-                }, {
-                    itemID: answer.pId
-                }
-            ], function(err, res) {
-                console.log("UPDATE res: "+res);
-                return;
-            }); 
-            /* connection.query(" UPDATE products SET  stockQuantity="+res[answer.pId -1].stockQuantity - answer.quantity+
-                    " where itemID="+answer.pId-1, function(err, res) {
-                        console.log("updated table");
-                console.log("UPDATE err: "+err);
-                //return; 
-                console.log("UPDATE res: "+res);
-            
-            });
-            */
-        }
-       // UPDATE products SET stockQuantity=13 where itemID=4;
+            console.log("Your total purchase price " , answer.quantity * res[answer.pId-1 ].price );
 
+            connection.query('UPDATE products,departments SET products.stockQuantity = ?, departments.totalSales = departments.totalSales + ? WHERE products.itemID = ? AND products.dept = departments.dept;',
+                [remainingStock, tprice,answer.pId],function(err){
+                    if(err) throw err;
+                    console.log(err);
+            
+                console.log('The total cost of purchase is ',tprice);
+            
+          
+               
+            }); 
+
+          
+            
+        }
+       
         
     })
 }) 
 
+            
 
 
-var toBuyItems = function(res) {
-    //console.log("res",res);
-    //console.log()
-    tempres=res;
-    inquirer.prompt([{
-            name: "pId",
-            type: "input",
-            message: "Please Enter Id of the Product you want to buy "},
-        {
-            name: "quantity",
-            type: "input",
-            message: "Please Enter number of Products you want to buy "   
-        },{
-            tempres2:tempres
-        }
-        ]).then(function(answer) {
-            //conole.log("tempre: ",tempres);
 
-        conole.log("tempres2: ",answer.tempres);
-            console.log("inside then");
-            console.log("answer.quantity ",answer.quantity);
-            console.log("tempres[pId-1].stockQuantity",tempres[pId-1].stockQuantity);
-
-            if (answer.quantity > tempres[pId].stockQuantity) {
-                console.log("Insufficient Quantity ");
-            }
-            else{
-                 connection.query("UPDATE products SET ? WHERE ?", [{
-                 stockQuantity:tempres[pId].stockQuantity - answer.quantity
-                 }, {
-                itemID: pId
-                }], function(err, res) {});
-
-
-            }
-
-        
-    })
-};
